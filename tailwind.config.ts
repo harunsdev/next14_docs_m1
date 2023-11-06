0ef0b01758906796690e4fa4b9f4fa854f9eab37
harunws <harunws33@gmail.com>
2023-11-06 13:30:41 +0100

    Chp#5 Completed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,15 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       gridTemplateColumns: {
-        '13': 'repeat(13, minmax(0, 1fr))',
+        "13": "repeat(13, minmax(0, 1fr))",
       },
       colors: {
         // blue: {
@@ -19,24 +19,25 @@ const config: Config = {
         // },
 
         orange: {
-          200: '#dd571c', //Fire
-          300: '#893101', //Amber
-          400: '#be5504', //Ginger
-          500: '#ed7014', //Bronze
-          600: '#b2560d', //Customized
-          1000: '#FF4F00', //International
+          100: "#fcd0bd",
+          200: "#dd571c", //Fire
+          300: "#893101", //Amber
+          400: "#be5504", //Ginger
+          500: "#ed7014", //Bronze
+          600: "#b2560d", //Customized
+          1000: "#FF4F00", //International
         },
       },
     },
     keyframes: {
       shimmer: {
-        '100%': {
-          transform: 'translateX(100%)',
+        "100%": {
+          transform: "translateX(100%)",
         },
       },
     },
   },
 
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require("@tailwindcss/forms")],
 };
 export default config;
