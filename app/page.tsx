0c3a38b9fac6ec1678d6cb33a09d93c07d9889df
harunws <harunws33@gmail.com>
2023-11-06 07:58:45 +0100

    Chp#2 Completed Now

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 import Link from "next/link";
+//TODO: Chp#2 Styles
+import styles from '@/app/ui/home.module.css'
 
 export default function Page() {
   return (
@@ -11,6 +13,8 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           {/*TODO: Chp#2 Figure black triangle */}
           <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
+          {/* TODO: Chp#2 Regular style */}
+          <div className={styles.shape}></div>
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
